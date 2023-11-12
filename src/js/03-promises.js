5ba0ee90015dd7fdc8237b35658c9fafdc4a65ef
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2023-11-12 15:26:53 +0100

    hw9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  
+  const amount = event.target.elements.amount.value;
+  const step = Number(event.target.elements.step.value);
+  let delay = Number(event.target.elements.delay.value);
+
+  for (let position = 1; position < amount; position++) {
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Report.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    delay += step;
+  };
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+}
+
+
+
+
+form.addEventListener('submit', onSubmitForm);
